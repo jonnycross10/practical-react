b3250dc31fb4c71fa135defedf92cab596eb29eb
Jonny Cross <crossjonny5@gmail.com>
2021-05-12 23:42:44 -0700

    Added some tags

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class P extends Component {
+    render() {
+        return (
+            <p>
+                {this.props.Title} {this.props.num}
+            </p>
+
+        );
+    }
+}
+
+const A = () => (
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Learn to code React
+    </a>
+    );
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+              <P Title="This is the title"></P>
+              <P Title="I learn more from personal projects"></P>
+              <P Title="Number of days wasted on degree: " num={720}></P>
+        <A></A>
       </header>
     </div>
   );
